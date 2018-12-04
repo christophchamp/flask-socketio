7b86e20eb9903433fde10ffc57bb202b348050c2
Christoph Champ <cchamp@redapt.com>
2018-12-04 15:03:31 -0800

    code cleanup

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'mysecret'
 socketio = SocketIO(app)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @socketio.on('message')
 def handleMessage(msg):
     print('Message: ' + msg)
     send(msg, broadcast=True)
 
+
 if __name__ == '__main__':
-    #socketio.run(app, host='0.0.0.0.', debug=True)
     socketio.run(app, debug=True)
